57f049ccc1c5a3415f2c077f2be38b9514c3951d
alekhyanairk <32680041+alekhyanairk@users.noreply.github.com>
2017-10-11 08:14:25 +0530

    Add files via upload
    First time upload

---
+++ DemoHelloWorld/DemoHelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+using System.Configuration;
+
+namespace DemoHelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string _apiurl = ConfigurationManager.AppSettings["APIURL"];
+            LoadData obj = new LoadData();
+            obj.HelloWorld(_apiurl);
+           
+        }
+    }
+  
+}
